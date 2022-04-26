0b319828baa8d741112156c67f9d9ddcf7dcd540
YounByoungil <jimmy7525@naver.com>
2022-04-27 00:26:53 +0900

    [feat] 커스텀 tableviewCell 생성

---
+++ NewsApp/NewsApp/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsApp
+//
+//  Created by 윤병일 on 2022/04/27.
+//
+
+import UIKit
+
+class ArticleTableViewCell : UITableViewCell {
+  
+  
+  @IBOutlet weak var titleLabel : UILabel!
+  @IBOutlet weak var descriptionLabel : UILabel! 
+}
