da37a69bd2f57e09c74655256e9da2a6e3fbe930
YounByoungil <jimmy7525@naver.com>
2022-05-06 02:41:47 +0900

    [feat] coffeeSizesSegmentedControl 코드로 구현

--- OrderCoffeeApp/OrderCoffeeApp/Controllers/AddOrderViewController.swift
+++ OrderCoffeeApp/OrderCoffeeApp/Controllers/AddOrderViewController.swift
@@ -13,7 +13,25 @@ class AddOrderViewController : UIViewController, UITableViewDelegate, UITableVie
   @IBOutlet weak var tableView : UITableView!
   
   private var viewModel = AddCoffeeOrderViewModel()
+  private var coffeeSizesSegmentedControl : UISegmentedControl!
   
+  //MARK: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+  }
+  
+  //MARK: - Functions
+  private func setupUI() {
+    self.coffeeSizesSegmentedControl = UISegmentedControl(items: self.viewModel.sizes)
+    self.coffeeSizesSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    
+    self.view.addSubview(self.coffeeSizesSegmentedControl)
+    
+    self.coffeeSizesSegmentedControl.topAnchor.constraint(equalTo: self.tableView.bottomAnchor, constant: 20).isActive = true
+    self.coffeeSizesSegmentedControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    
+  }
   
   //MARK: - UITableViewDataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
