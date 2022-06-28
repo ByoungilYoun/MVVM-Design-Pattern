8a12ce7cfe161c3bc8004ab3f1c439ef97a0c8d2
YounByoungil <jimmy7525@naver.com>
2022-06-28 23:32:47 +0900

    [UI] set UI

--- RxSwift+MVVM/RxSwift+MVVM/ViewController.swift
+++ RxSwift+MVVM/RxSwift+MVVM/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+  
+  //MARK: - Properties
+  private let timerLabel : UILabel = {
+    let label = UILabel()
+    label.textColor = .black
+    return label
+  }()
+  
+  let loadButton : UIButton = {
+    let button = UIButton()
+    button.setTitle("Load", for: .normal)
+    button.backgroundColor = .black
+    button.setTitleColor(UIColor.white, for: .normal)
+    button.titleLabel?.textColor = .black
+    return button
+  }()
 
+  let editView : UITextView = {
+    let view = UITextView()
+    view.textColor = .black
+    view.backgroundColor = .lightGray
+    return view
+  }()
+  
+  //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    layout()
   }
 
-
+  private func layout() {
+    view.backgroundColor = .white
+    
+    [timerLabel, loadButton, editView].forEach {
+      view.addSubview($0)
+    }
+    
+    timerLabel.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.leading.trailing.equalToSuperview().inset(15)
+      $0.height.equalTo(30)
+    }
+    
+    loadButton.snp.makeConstraints {
+      $0.top.equalTo(timerLabel.snp.bottom).offset(10)
+      $0.leading.trailing.equalTo(timerLabel)
+      $0.height.equalTo(50)
+    }
+    
+    editView.snp.makeConstraints {
+      $0.top.equalTo(loadButton.snp.bottom).offset(10)
+      $0.leading.trailing.equalTo(timerLabel)
+      $0.bottom.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
 }
 
